e14e34b348a866ba8a9d28f5dfea83e81a3087e9
DiogoRibeiro7 <diogo_dj@hotmail.com>
2021-09-30 13:04:08 +0100

    trials

--- test_new_pytest.py
+++ test_new_pytest.py
@@ -1,20 +1,22 @@
 import pandas as pd
+import pytest 
 
-def load_data():
-    data = pd.DataFrame([[0, 2, 7, 4, 8],
-                         [1, 7, 6, 3, 7],
-                         [1, 1, "None", 8, 9],
-                         [0, 2, 3, "None", 6],
-                         [0, 5, 1, 4, 9]], 
-                        columns = [f"feature_{i}" if i!=0 
-                                  else "class" for i in range(5)])
-    return data
+@pytest.fixture(scope='module')
+def data():
+    df = pd.DataFrame([[0, 2, 6, 4, 8],
+                         [1, 7, 5, 3, 7],
+                         [1, 1, 1, 8, 9],
+                         [0, 2, 3, 3, 6],
+                         [0, 5, 3, 4, 9]], 
+                        columns = [f'feature_{i}' if i!=0 
+                                  else 'class' for i in range(5)])
+    return df
+
 
 def aggregate_mean(df, column):
     return df.groupby("class")[column].mean().to_dict()
 
-def test_aggregate_mean_feature_1():   
-    data = load_data()
-    expected = {0: 3, 1: 4}
-    result = aggregate_mean(data, "feature_1")
-    assert expected == result
+
+@pytest.mark.parametrize("column, expected", [("feature_1", {0: 3, 1: 4}), ("feature_2", {0: 4, 1: 3})])
+def test_aggregate_mean_feature_1(data, column, expected):   
+    assert expected == aggregate_mean(data, column)
